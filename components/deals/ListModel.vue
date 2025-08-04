5a0cd2edd5280cb7064d666d63328d1d18ab76a8
Hafiz <abdulmoustapha64@@gmail.com>
2025-08-04 10:18:03 +0100

    Best Deals Updated

--- components/deals/ListModel.vue
+++ components/deals/ListModel.vue
@@ -102,21 +102,12 @@ const props = defineProps({
   mPercent: Number,
 })
 
-// Référence pour stocker la valeur qui ne doit pas changer
-const bestMatchingCar = ref(null)
-
-// Calcul initial au montage
-onMounted(() => {
-  bestMatchingCar.value = props.car.comparisons?.find(caro => 
-    caro.matching_percentage >= props.mPercent
-  ) || null
-})
-
-// const bestMatchingCar = props.car.comparisons?.find(caro => caro.matching_percentage >= props.mPercent) || null;
-
-// const bestMatchingCar = computed(() => {
-//   return props.car.comparisons?.find(caro => caro.matching_percentage >= props.mPercent) || null
-// })
+// 
+const bestMatchingCar = computed(() => {
+  return props.car.comparisons?.find(caro =>
+    caro.matching_percentage >= (props.mPercent ?? 0)
+  ) || null;
+});
 
 defineEmits(['view'])
 </script>
